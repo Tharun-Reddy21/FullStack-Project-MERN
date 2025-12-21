8b0635dc467abe39c68e561bc497ec57d637b1f5
Tharun-Reddy21 <tarun3run@gmail.com>
2025-12-21 12:09:19 +0530

    installed react project setup with vite and tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex justify-around pt-4 text-3xl font-bold">
+        <h1>Mern Blog Project</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
